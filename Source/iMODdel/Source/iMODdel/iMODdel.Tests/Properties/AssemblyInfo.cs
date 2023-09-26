4aeb17206010c313d01097f6bc375367640823c0
Koen van der Hauw <koen.vanderhauw@sweco.nl>
2023-09-26 17:27:23 +0200

    - Updated to latest libraries and increased version number

--- Source/iMODdel/Source/iMODdel/iMODdel.Tests/Properties/AssemblyInfo.cs
+++ Source/iMODdel/Source/iMODdel/iMODdel.Tests/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("bd59a2c0-0025-40ab-93e1-754919573de4")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
